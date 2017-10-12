00ca170e81d45248adc91071501cf64c1a1e6cbb
YEBIN <474370149@qq.com>
2017-10-12 22:02:48 +0800

    123

---
+++ demo1.java
@@ -0,0 +1,30 @@
+class Demo
+{
+	public static void main(String[] args)
+	{
+	//  int x=3;
+	//	byte b=4;
+	//  b=b+3;
+	
+	//	byte b=3;
+	//	b=(byte)(b+200);//强制类型转换
+	
+	//	System.out.println((char)('a'+1));
+	//  System.out.println('你'+0);//unicode国际标准码表
+		
+		
+		byte b=4;
+		//b=3+7;
+		byte b1=3;
+		byte b2=7
+		b=b1+b2;//会编译错误 b1b2为变量可能会溢出 应定义一个int型
+		
+		
+		
+		
+		
+		
+		
+		System.out.println("hello world");
+	}
+}
